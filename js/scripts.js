0af5d92ecb59a46f910f9e2837e8f0ef9d295a01
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-17 20:35:47 -0700

    add array.froEach, return Array, .reverse and .join
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,19 @@ $(document).ready(function(){
   $("form#wordPlay").submit(function(event) {
     event.preventDefault();
     const array = $("input#sentence").val();
+    const sentence = $("input#sentence").val();
+    let sentenceArray = sentence.split(" ");
+    let newArray = []
+    sentenceArray.forEach(function(word) {
+      if (word.length >= 2) {
+        newArray.push(word);
+      }
+      return newArray;
+    })
+    $("#userSentence").append(newArray.join(" "));
+    $("#userSentence").append("<br>");
+    $("#userSentence").append("<strong>Reverse Echo:</strong>");
+    $("#userSentence").append("<br>");
+    $("#userSentence").append(newArray.reverse().join(" "));
   });
 });
